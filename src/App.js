d4d8c43d2479a77369deee921edc1c1831c78506
Tawseef-Reza <65203327+Tawseef-Reza@users.noreply.github.com>
2023-03-07 17:03:30 -0500

    made changes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,57 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
+            <nav>
+                <div className="header-container"> 
+                    <div className="header-container-left">
+                        EcoVacations
+                    </div>
+                    <div className="header-container-line">
+
+                    </div>
+                    <div className="header-container-right">
+                        <div>
+                            My Favorites
+                        </div>
+                        <div>
+                            Menu
+                        </div>
+                        <div>
+                            About Us
+                        </div>
+                        <div>
+                            Account Name
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div className="main-search">
+                <div className="main-search-title">
+
+                </div>
+                <div className="main-search-text">
+
+                </div>
+                <div className="main-search-bar">
+
+                </div>
+            </div>
+
+
+
+            {/* <header className="App-header">
                 <button onClick={callApi}>Call API</button>
                 <p id="first-paragraph"></p>
                 <p>Hello world!</p>
-            </header>
+            </header> */}
         </div>
     );
 }
 
-function callApi() {
-    fetch('http://localhost:3001/details', { method: 'GET' })
-        .then(data => data.json())
-        .then(json => (document.getElementById("first-paragraph").innerHTML = json["data"]))
-}
+// function callApi() {
+//     fetch('http://localhost:3001/details', { method: 'GET' })
+//         .then(data => data.json())
+//         .then(json => (document.getElementById("first-paragraph").innerHTML = json["data"]))
+// }
 
 export default App;
