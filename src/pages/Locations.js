000c6e32d147c458cdf9db4f5b451c4f76e450b4
Tawseef-Reza <65203327+Tawseef-Reza@users.noreply.github.com>
2023-03-21 17:39:44 -0400

    new branch for testing backend calling

--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -2,12 +2,35 @@ import React from 'react';
 import Layout from "./Layout";
 
 const Locations = () => {
+  let apiData = callAPI();
+
   return (
     <div>
       <Layout />
       <h1>Locations Page Works</h1>
+      <input id="locationInput" placeholder='Type a location here...'></input>
+      <button onClick={callAPI}>Submit Location</button>
+      <h1 className="holder"> {apiData.date},{apiData.aqi},{apiData.gas}</h1>
     </div>
   );
 };
-  
+
+var date = "";
+var aqi = "";
+var gas = "";
+function callAPI() {
+  const locationSubmission = document.getElementById("locationInput").value;
+  fetch(`http://localhost:3001/location/${locationSubmission}`, { method: 'GET' })
+
+  .then(data => data.json())
+  .then(json => {
+    console.log(json);
+    return {
+      date: date,
+      aqi: aqi,
+      gas: gas
+    }
+  })
+}
+
 export default Locations;
